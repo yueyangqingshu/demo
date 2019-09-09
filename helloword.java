c94a67089a50e8d138f4b07a1a4447d561a10d05
yueyangqingshu <1819409088@qq.com>
2019-09-09 22:28:57 +0800

    hello word

---
+++ helloword.java
@@ -0,0 +1,9 @@
+public class Helloword{
+public static void main(String[] agrs){
+System.out.println("1")
+	System.out.println("helloword");
+System.out.println("2");
+	System.out.println("helloword");
+
+}
+}

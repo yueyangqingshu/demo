849701f4c34100555dcba65b30f5b13ae039e7d8
yueyangqingshu <1819409088@qq.com>
2019-09-10 08:19:02 +0800

    hell

--- helloword.java
+++ helloword.java
@@ -4,13 +4,6 @@ public static void main(String[] agrs){
 	System.out.println("helloword");
 System.out.println("1")
 	System.out.println("helloword");
-System.out.println("2");
-=======
-System.out.println("1")
-	System.out.println("helloword");
-System.out.println("2");
-	System.out.println("helloword");
+System.out.println("2222");
 
->>>>>>> origin/branch2
-}
 }

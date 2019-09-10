b6064b6ea2faca58e786fe8c576a53274c0363dd
yueyangqingshu <1819409088@qq.com>
2019-09-10 20:59:45 +0800

    commit

--- helloword.java
+++ helloword.java
@@ -1,9 +1,6 @@
 public class Helloword{
 public static void main(String[] agrs){
-<<<<<<< HEAD
 	System.out.println("helloword");
-System.out.println("1")
-	System.out.println("helloword");
-System.out.println("2222");
+
 
 }

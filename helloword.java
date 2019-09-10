b73a21a224a5aaa4f4d6c2af32af291c5147833f
yueyangqingshu <1819409088@qq.com>
2019-09-10 08:08:18 +0800

    hllowword

--- helloword.java
+++ helloword.java
@@ -1,16 +1,8 @@
 public class Helloword{
 public static void main(String[] agrs){
-<<<<<<< HEAD
 	System.out.println("helloword");
 System.out.println("1")
 	System.out.println("helloword");
-System.out.println("2");
-=======
-System.out.println("1")
-	System.out.println("helloword");
-System.out.println("2");
-	System.out.println("helloword");
+System.out.println("5646456456Ww");
 
->>>>>>> origin/branch2
-}
 }
